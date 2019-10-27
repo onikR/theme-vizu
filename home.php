800009d21c5d024b899a8f9d478a4bfcf380a829
Onik Rahman <rahman10308@he-ferrer.eu>
2019-10-27 13:16:15 +0100

    Update

--- home.php
+++ home.php
@@ -1,5 +1,16 @@
 <?php require("header.php");?>
 
-<div class="btn btn-success">Onik</div>
+<div class="signal d-flex justify-content-center align-items-center">No signal</div>
+<div class="container-fluid">
+<div class="row">
+    <div class="blue col-3"></div>
+    <div class="orange col-3"></div>
+    <div class="pink col-3"></div>
+    <div class="green col-3"></div>
+
+    </div>
+
+
+</div>
 
 <?php require("footer.php");?>
